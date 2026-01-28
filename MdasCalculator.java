3f2fcd69207680447d905782cf4def97eea04787
dredo <dreddmarco09@gmail.com>
2026-01-28 23:25:41 +0800

    MultiplicationOne
    Implemented Multiplication logicy

--- MdasCalculator.java
+++ MdasCalculator.java
@@ -6,7 +6,9 @@ public class MdasCalculator {
 
         Scanner sc = new Scanner(System.in);
 
-        int num1, num2, result;
+        int num1 = 0;
+        int num2 = 0;
+        int result = 0;
         char operator;
 
         System.out.print("Enter first number: ");
@@ -17,9 +19,17 @@ public class MdasCalculator {
         
         System.out.print("Enter operator (+, -, *, /): ");
         operator = sc.next().charAt(0);
-
+        
+        switch(operator){
+            case '*' -> result = Multiplication(num1, num2);
+        }
+        System.out.println("Result: " + result);
         sc.close();
     }
 
+    public static int Multiplication(int num1, int num2){
+        return num1 * num2;
+    }
 
+    
 }
